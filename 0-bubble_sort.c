42c8fb2df2e9a141a99db1e05d28060f80221dfd
sagexsane <sanelisiwebhengu01@gmail.com>
2024-05-01 00:08:12 +0200

    Add requirement task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,7 +20,8 @@ void bubble_sort(int *array, size_t size)
 				temp = *(array + b);
 				*(array + b) = *(array + b + 1);
 				*(array + b + 1) = temp;
-				print_array(array, size);
+				if size >= 2
+					print_array(array, size);
 			}
 		}
 	}

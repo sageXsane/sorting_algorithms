f3f68bfe1800a0b7de14e92b72821af2a66185a7
sagexsane <sanelisiwebhengu01@gmail.com>
2024-05-01 00:10:42 +0200

    Add requirement task 0(2)

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,6 +11,8 @@ void bubble_sort(int *array, size_t size)
 	size_t a, b;
 	int temp;
 
+	if size < 2
+		return;
 	for (a = 0; a < size; a++)
 	{
 		for (b = 0; b < size - 1; b++)
@@ -20,8 +22,7 @@ void bubble_sort(int *array, size_t size)
 				temp = *(array + b);
 				*(array + b) = *(array + b + 1);
 				*(array + b + 1) = temp;
-				if size >= 2
-					print_array(array, size);
+				print_array(array, size);
 			}
 		}
 	}
